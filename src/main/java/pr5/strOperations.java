19249c58289841e175fc2f86da645aa841de7317
Anton <tony.lipchansky@gmail.com>
2021-10-25 15:34:19 +0300

    new Pr

---
+++ src/main/java/pr5/strOperations.java
@@ -0,0 +1,43 @@
+package pr5;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class strOperations {
+
+	public static String InsertLetter(String x) throws IOException {
+		
+
+		ByteArrayOutputStream newString = new ByteArrayOutputStream();
+
+		byte[] splitoByte = x.getBytes();
+
+		for (int i = 0; i < splitoByte.length; i++) {
+
+			newString.write(splitoByte[i]);
+
+			if (splitoByte[i] == 's') {
+
+				newString.write('f');
+
+			}
+
+			newString.close();
+
+		}
+
+		return newString.toString();
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		String str = "Tests";
+		
+		
+		InsertLetter(str);
+		
+
+	}
+
+}
